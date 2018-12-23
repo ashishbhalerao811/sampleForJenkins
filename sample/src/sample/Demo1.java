c346e03d5b563de50f07045adca4643c10ea2476
Ashish A. Bhalerao <Ashish A. Bhalerao@DELL>
2018-12-23 12:12:03 +0530

    Added samplProject for Jenkins.

---
+++ sample/src/sample/Demo1.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+	@Test
+	public void sum() 
+	{
+		System.out.println("Welcome to the Jenkins World");
+	}
+
+}
