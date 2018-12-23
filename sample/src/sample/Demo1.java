6f661a26cfeef188cf86e7b82cbfab1aac905fc7
Ashish A. Bhalerao <Ashish A. Bhalerao@DELL>
2018-12-23 12:37:19 +0530

    changed method to static method.

--- sample/src/sample/Demo1.java
+++ sample/src/sample/Demo1.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class Demo1 {
 
 	
-	public void sum() 
+	public static  void main(String [] args) 
 	{
 		System.out.println("Welcome to the Jenkins World");
 	}
